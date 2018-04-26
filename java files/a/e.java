31fb4a3b59d8448cc6e75a37b5395729d22cf8fa
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2018-04-26 20:08:13 +0900

    analysis e.java

--- java files/a/e.java
+++ java files/a/e.java
@@ -2,11 +2,11 @@ package a;
 
 public final class e extends a {
    private e(b var1) {
-      super(c.f, var1);
+      super(c.f, var1); // c.f는 Unknown
    }
 
    public e() {
-      this(b.a());
+      this(b.a()); // b.a()는 architecture를 반환. this(b.a())는 (OS, arch)로 추상화
    }
 
    public final String b() {
